ff0667378649df9bcbe9488a164a99d0e671694d
Mr-Leshiy <leshiy12345678@gmail.com>
2022-03-16 21:56:15 +0200

    import polkadot keys

--- src/polka/wallet.js
+++ src/polka/wallet.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './wallet.css';
+import {
+    web3Accounts,
+    web3Enable,
+  } from '@polkadot/extension-dapp';
 
 class PolkaWallet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            accounts: null,
+        };
+    }
+
+    async onClick() {
+        // returns an array of all the injected sources
+        // (this needs to be called first, before other requests)
+        const allInjected = await web3Enable('nft-tickets dapp');
+
+        // returns an array of { address, meta: { name, source } }
+        // meta.source contains the name of the extension that provides this account
+        const accounts = await web3Accounts();
+
+        this.state.accounts = accounts;
+        this.setState(this.state);
+    }
+
     render() {
         return (
-            <button className="integrate-button"> 
-                Integrate Polkadot extension
+            <button className="integrate-button" onClick={ () => { this.onClick(); } }> 
+                Polkadot
             </button>
         );
     }
