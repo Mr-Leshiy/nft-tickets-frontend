efc67f23568e9e39a4aaad064c973c7441884ef3
Alex Pozhylenkov <leshiy12345678@gmail.com>
2022-03-19 22:20:29 +0200

    Add bootstrap dep (#10)
    * add bootstrap dep
    
    * update project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
 import "./App.css";
-import PolkaWallet from "./polka/wallet";
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-integration">
-          <PolkaWallet />
-        </div>
-      </div>
-    );
+    return <div></div>;
   }
 }
 
