4252e8178d0e61c4f0e1d6066a07c7001107f4e4
Alex Pozhylenkov <leshiy12345678@gmail.com>
2022-03-19 21:26:11 +0200

    add initial CI (#8)
    * add initial CI
    
    * fix CI

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import PolkaWallet from './polka/wallet';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="App">
