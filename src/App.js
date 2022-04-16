6f18a1aa4f3d6c73faa3b02ec19304a5837eecf7
Alex Pozhylenkov <leshiy12345678@gmail.com>
2022-04-16 21:48:27 +0300

    Update Buttons styling (#15)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import Header from "./components/Header/Header";
 import AboutUs from "./components/Pages/AboutUs/AboutUs";
 import Tickets from "./components/Pages/Tickets/Tickets";
