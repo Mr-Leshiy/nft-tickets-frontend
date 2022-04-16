ed8edec3ad0732319c906537f823818f9e22ed47
Mr-Leshiy <leshiy12345678@gmail.com>
2022-04-16 17:55:44 +0300

    add new Buttons

--- src/components/Pages/Tickets/Tickets.js
+++ src/components/Pages/Tickets/Tickets.js
@@ -1,9 +1,15 @@
 import Page from "../../UI/Page";
+import Button from "../../UI/Button";
+import styles from "./Tickets.module.css";
 
 const Tickets = (props) => {
   return (
     <Page>
       <h1>Tickets</h1>
+      <div className={styles.bar}>
+        <Button>Create Tickets</Button>
+        <Button>Send Tickets</Button>
+      </div>
     </Page>
   );
 };
