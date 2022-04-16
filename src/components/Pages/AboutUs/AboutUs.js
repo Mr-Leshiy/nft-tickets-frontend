3167d464840a9504636fa607cbf654faf29c6676
Mr-Leshiy <leshiy12345678@gmail.com>
2022-04-16 13:14:14 +0300

    add pages changing

---
+++ src/components/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,11 @@
+import styles from "./AboutUsPage.module.css";
+
+const AboutUs = (props) => {
+  return (
+    <div className={styles.page}>
+      <h1>About us</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/Pages/Tickets/Tickets.js
@@ -0,0 +1,11 @@
+import styles from "./Tickets.module.css";
+
+const Tickets = (props) => {
+  return (
+    <div className={styles.page}>
+      <h1>Tickets</h1>
+    </div>
+  );
+};
+
+export default Tickets;
