f7b171afe31d2a8efed77f0f4d008b76d53101ee
Mr-Leshiy <leshiy12345678@gmail.com>
2022-04-17 13:15:10 +0300

    add CreateTickets form

--- src/components/Pages/Tickets/CreateTickets.js
+++ src/components/Pages/Tickets/CreateTickets.js
@@ -1,10 +1,20 @@
 import Button from "../../UI/Button";
+import Input from "../../UI/Input";
 import Modal from "../../UI/Modal";
+import styles from "./CreateTickets.module.css";
 
 const CreateTickets = (props) => {
   return (
     <Modal>
-      <Button input={{ onClick: props.onHide }}>Close</Button>
+      <div>
+        <form>
+          <Input name="Collection name" input={{}}></Input>
+        </form>
+        <div className={styles.buttons}>
+          <Button input={{ onClick: props.onHide }}>Close</Button>
+          <Button input={{ onClick: props.onHide }}>Apply</Button>
+        </div>
+      </div>
     </Modal>
   );
 };
